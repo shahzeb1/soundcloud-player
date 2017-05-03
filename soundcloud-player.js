734459c32bd80f4e7bdb1c75958004c2c45b4751
Victor Akinyemi <hadokoda@yahoo.com>
2017-05-03 01:14:27 +0100

    Implemented module loader compatibility.
    Falls back to adding an "SCPlayer" object to the global namespace.

--- soundcloud-player.js
+++ soundcloud-player.js
@@ -1,13 +1,12 @@
+(function () {
 /**
   Loads song from SoundCloud by replacing the iFrame #so on the page and optionally autoplays it.
   @param {string} song - The song URL from soundcloud.com/...
-  @param {bool} autoPlay - Should it autoplay the song after loading it.
-*/
+  @param {bool} autoPlay - Should it autoplay the song after loading it.*/
 function loadFromSC(song, autoPlay) {
   // Set up URL
   var uri = encodeURIComponent(song);
-  var scUrl ='https://w.soundcloud.com/player/?url=' + uri;
-  // Set iFrame source
+  var scUrl ='https://w.soundcloud.com/player/?url=' + uri;  // Set iFrame source
   $("#so").prop('src', scUrl);
   // Play song after 1 sec delay
   $("#so").on("load", function () {
@@ -67,3 +66,22 @@ function toggleIsPlaying() {
         });
     });
 };
+var SCPlayer = {
+    "loadFromSC": loadFromSC,
+    "getSCinfo": getSCinfo,
+    "play": play,
+    "pause": pause,
+    "toggleIsPlaying": toggleIsPlaying
+};
+// Detects AMD-compatible loaders and Node.js environments
+if (module && module.exports) {
+    module.exports = SCPlayer;
+}
+else if (module && exports) { // CommonJS
+    exports.SCPlayer = SCPlayer;
+}
+else {
+   window.SCPlayer = SCPlayer; // ES6: export SCPlayer;
+}
+})();
+    
